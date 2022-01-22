49d8b587ae91b9512acc2de96c8eaf6ffc21797d
karim732 <82142992+karim732@users.noreply.github.com>
2022-01-22 22:38:25 +0530

    Add files via upload

---
+++ quizscript.js
@@ -0,0 +1,36 @@
+
+
+// Array of objects
+const quiz = [
+    {
+        q:' A² + b² + c² + 2ab + 2bc + 2ca is equal to',
+        options:['(a + b - c)²','(a + b + c)²','2abc (a + b + c)²','(a - b + c)²'],
+        answer:1
+    },
+    {
+        q:' If a + b = 5 and a - b = 3 then the value of a² + b² should be',
+        options:['21','15','17','11'],
+        answer:2
+    },
+    {
+        q:'The number of methods to find H.C.F are',
+        options:['2','4','3','5'],
+        answer:0
+    },
+    {
+        q:'Product of two expressions is equal to',
+        options:['H.C.F','L.C.M','H.C.F+L.C.M','H.C.F*L.C.M'],
+        answer:3
+    },
+    {
+        q:'Area of a triangle with base b and height h is represented by',
+        options:['1⁄2 × bh',
+            '2 + bh',
+            '2 × bh',
+            '2⁄bh'],
+        answer:0
+    }
+    
+    
+
+]
